47d63ba21cdbd92e342b8fbd1cb39de7fa3eceaa
구본욱 Finn <bnoo1333@gmail.com>
2025-04-20 14:32:42 +0900

    Project 생성 (#1)
    * Create Project
    
    * Add Sample Application
    
    * Update Readme

---
+++ RichTextEditor/RichTextEditor/RichTextEditor.h
@@ -0,0 +1,18 @@
+//
+//  RichTextEditor.h
+//  RichTextEditor
+//
+//  Created by 구본욱 on 4/20/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RichTextEditor.
+FOUNDATION_EXPORT double RichTextEditorVersionNumber;
+
+//! Project version string for RichTextEditor.
+FOUNDATION_EXPORT const unsigned char RichTextEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RichTextEditor/PublicHeader.h>
+
+
---
+++ RichTextEditor/RichTextEditorSample/RichTextEditorSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichTextEditorSampleApp.swift
+//  RichTextEditorSample
+//
+//  Created by 구본욱 on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct RichTextEditorSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
